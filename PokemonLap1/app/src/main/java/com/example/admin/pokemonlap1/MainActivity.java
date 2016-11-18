f6687b663f95232dc293ed04eb2573bffacd5848
DESKTOP-TVFEE04\Admin <nguyenquocviet1306@gmail.com>
2016-11-18 13:31:10 +0700

    pokemonLap1

---
+++ PokemonLap1/app/src/main/java/com/example/admin/pokemonlap1/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.admin.pokemonlap1;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.example.admin.pokemonlap1.fragment.SettingFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.ivPlay)
+    ImageView ivPlay;
+    @BindView(R.id.btSetting)
+    Button btSetting;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+    private void changeFragment(Fragment fragment, boolean addToBackStack){
+        FragmentTransaction fragmentTransaction =
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.activity_main,fragment);
+        if (addToBackStack) {
+            fragmentTransaction.addToBackStack(null);
+        }
+
+        fragmentTransaction.commit();
+    }
+
+    @OnClick(R.id.btSetting)
+    public void SettingClick(Button button){
+        changeFragment(new SettingFragment(),true);
+    }
+
+
+}
